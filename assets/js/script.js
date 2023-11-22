545840a33def1a56d6304ced7c3ce0b884dd5b31
aedoardo1990 <>
2023-11-22 17:52:06 +0000

    photo from submit form to div realized successfully. temporarily deleted photos of pilots to clear working space

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,15 @@ function closeModal() {
     dialog.style.display = "none";
 }
 
+// transmitting Photo to Div with class="card-driver" in HTML - credit to https://stackoverflow.com/questions/71608101/how-to-fill-a-div-with-an-image-the-user-uploads
+const img = document.getElementById('tosubmit')
+const input = document.getElementById('driver-photo-input');
+input.onchange = function (ev) {
+    const file = ev.target.files[0]; // get the file
+    const blobURL = URL.createObjectURL(file);
+    img.src = blobURL;
+}
+
 
 // Toggle On Off created thanks to https://stackoverflow.com/questions/55018585/how-to-turn-on-audio-on-click-icon-play-pause
 function togglePlay() {
